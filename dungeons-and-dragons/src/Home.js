11fde1e56cb84232500afcf743918cffecc8cef8
ClaytonC <cjcanady1113@gmail.com>
2020-07-28 14:40:10 -0400

    spell list now pulls data from API

--- dungeons-and-dragons/src/Home.js
+++ dungeons-and-dragons/src/Home.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import SpellList from './SpellList';
 class Home extends Component {
     render() {
         return (
--- dungeons-and-dragons/src/SpellList.js
+++ dungeons-and-dragons/src/SpellList.js
@@ -1,23 +1,36 @@
 import React, { Component } from 'react';
 import { Route, Link } from 'react-router-dom';
-import data from './data'
+// import data from './data'
 class SpellList extends Component {
-    render() {
-        let list = data.map((spell, index) => {
-return (
-	<div className='spell' spell={spell[index]}>
-		<Link to={/info/ + spell.name} spell={spell[index]} >
-			<h1>{spell.name}</h1>
-		</Link>
-	</div>
-);
-        }) 
-        return (
-            <div className='spell-container'>
-                {list}
-            </div>
-        );
+    constructor() {
+        super();
+        this.state = {
+            spells: []
+        };
     }
+	componentDidMount() {
+		fetch('https://www.dnd5eapi.co/api/spells')
+			.then((results) => results.json())
+			.then((results) => {
+				 this.setState({ spells: results.results})
+			})
+			.catch((err) => {
+				console.error(err);
+			});
+	};
+	render() {
+        console.log(this.state.spells);
+		let list = this.state.spells.map((spell, index) => {
+			return (
+				<div className='spell' spell={spell[index]} key={index}>
+					<Link to={/info/ + spell.name} spell={spell[index]}>
+						<h1>{spell.name}</h1>
+					</Link>
+				</div>
+			);
+		});
+		return <div className='spell-container'>{list}</div>;
+	}
 }
 
 export default SpellList;
