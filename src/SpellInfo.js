027fa114029fa3167bd993d0b679e690d220098d
ClaytonC <cjcanady1113@gmail.com>
2020-07-28 18:27:11 -0400

    spell info now displays spell name and desc

--- src/SpellInfo.js
+++ src/SpellInfo.js
@@ -1,13 +1,31 @@
 import React, { Component } from 'react';
 
 class SpellInfo extends Component {
-	render() {
+	constructor() {
+		super();
+		this.state = {
+			spell: [],
+		};
+	}
+	componentDidMount() {
 		const thisSpell = this.props.spells.filter((spell) => {
 			return spell.name === this.props.match.params.spellName;
 		})[0];
+		fetch(`https://www.dnd5eapi.co${thisSpell.url}`)
+			.then((results) => results.json())
+			.then((results) => {
+				this.setState({ spell: results });
+			})
+			.catch((err) => {
+				console.error(err);
+			});
+	}
+	render() {
+		console.log(this.state.spell);
 		return (
 			<div className='spell-info'>
-				<h1>{thisSpell.name}</h1>
+				<h2>{this.state.spell.name}</h2>
+				<p>{this.state.spell.desc}</p>
 			</div>
 		);
 	}
--- src/SpellList.js
+++ src/SpellList.js
@@ -14,6 +14,7 @@ class SpellList extends Component {
 				</div>
 			);
 		});
+		
 		return (
 			<div className='spell-container'>
 				{list}
