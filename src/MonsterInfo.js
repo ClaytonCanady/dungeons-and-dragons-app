6a61f1523496421ebb37973682d7c709d0710ed3
ClaytonC <cjcanady1113@gmail.com>
2020-07-29 13:33:53 -0400

    added ul with creature stats

--- src/MonsterInfo.js
+++ src/MonsterInfo.js
@@ -26,11 +26,23 @@ class MonsterInfo extends Component {
 			<div className='info'>
 				<h2>{this.state.monster.name}</h2>
 				<h4>
-		This size {this.state.monster.size} {this.state.monster.type} has a challenge rating of {this.state.monster.challenge_rating} and an Alignment of {this.state.monster.alignment}.
+					This size {this.state.monster.size} {this.state.monster.type} has a
+					challenge rating of {this.state.monster.challenge_rating} and an
+					Alignment of {this.state.monster.alignment}.
 				</h4>
 				<p>
-					This creature's languages are {this.state.monster.languages || 'none'}.
+					This creature's languages are {this.state.monster.languages || 'none'}
+					.
 				</p>
+				<h4>Stats</h4>
+				<ul>
+					<li>Strength: {this.state.monster.strength}</li>
+					<li>Constitution: {this.state.monster.constitution}</li>
+					<li>Dexterity: {this.state.monster.dexterity}</li>
+					<li>Intelligence: {this.state.monster.intelligence}</li>
+					<li>Wisdom: {this.state.monster.wisdom}</li>
+					<li>Charisma: {this.state.monster.charisma}</li>
+				</ul>
 			</div>
 		);
 	}
